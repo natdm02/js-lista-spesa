d1778ccbff55979152b62587d050a519707178ad
natdm02 <natalidimarino2002>
2024-02-27 15:04:46 +0100

    [add] punto 1 e 2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// 1. 
+const items = ['Latte','Uova','Pane','Carne','Acqua','Fazzoletti',];
+
+// 2.
+console.log('items', items, items.length, typeof items);
+
+const itemsList = document.getElementById('items');
+console.log('itemsList', itemsList, typeof itemsList);
+
+let i = 0;
+
