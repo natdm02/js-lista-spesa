e0636a56e4034da142acb2756b013293a0ce53b0
natdm02 <natalidimarino2002>
2024-02-27 15:24:55 +0100

    [add] 3 e 4

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // 1. 
+
 const items = ['Latte','Uova','Pane','Carne','Acqua','Fazzoletti',];
 
 // 2.
+
 console.log('items', items, items.length, typeof items);
 
 const itemsList = document.getElementById('items');
@@ -9,3 +11,18 @@ console.log('itemsList', itemsList, typeof itemsList);
 
 let i = 0;
 
+// 3 
+
+while (i < items.length) {
+
+  console.log(i, items[i]);
+
+  const newLi = document.createElement('li');
+  newLi.innerHTML = items[i];
+  itemsList.append(newLi);
+
+// 4 
+
+  i++
+
+}
